f71ed1923a2c18deaa7090b6a8d97e331fed2129
Joseph Ntaimo <jntaimo@gmail.com>
2019-09-14 20:21:28 -0400

    um

--- handicap.py
+++ handicap.py
@@ -2,16 +2,16 @@ import re
 f = open('HandicapCoordinates.txt', 'r')
 f_line = f.readlines()
 
-dictionaryList = []
+positions = []
 
 for x in f_line:
     long = re.search('long_wgs84: (-\d\d\.\d*)', x)
-    dictionaryList.append(long.group(1))
+    positions.append(long.group(1))
 
     lat = re.search('lat_wgs84: (\d\d\.\d*)', x)
-    dictionaryList.append(lat.group(1))
+    positions.append(lat.group(1))
 
-#print(dictionaryList)
+print(positions)
 #print(len(dictionaryList))
 
 
